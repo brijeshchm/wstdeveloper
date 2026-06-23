ef24008b67d2590b5e69faf165be5609fa8bbea1
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-06-23 18:12:37 +0530

    blog

--- resources/views/site/layouts/app.blade.php
+++ resources/views/site/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>@yield('title')</title>
-<meta name="keywords" content="@yield('keyword')">
+<meta name="keywords" content="@yield('keywords')">
 <meta name="description" content="@yield('description')">
 <meta name="csrf-token" content="<?php echo csrf_token(); ?>">
 <meta name="google-site-verification" content="LFZ78S5IvrX1soxewYLtjXZXo8gCefgIMMJAtgf4QuM" /> 
