a8dbcd723140c3b891ada82fa68533eabe1f272d
Brijesh Chauhan <36526591+brijeshchm@users.noreply.github.com>
2026-08-25 10:27:19 +0530

    caree

--- routes/web.php
+++ routes/web.php
@@ -55,7 +55,7 @@ Route::get('/terms-conditions', [App\Http\Controllers\Site\HomeController::class
 Route::get('/privacy-policy', [App\Http\Controllers\Site\HomeController::class, 'privacypolicy'])->name('privacy.policy');
 Route::get('/copyright-policy', [App\Http\Controllers\Site\HomeController::class, 'copyrightpolicy'])->name('copyright.policy');
 Route::get('/refund-policy', [App\Http\Controllers\Site\HomeController::class, 'refundPolicy'])->name('refund.policy');
-//Route::get('/careers', [App\Http\Controllers\Site\HomeController::class, 'careers'])->name('careers');
+Route::get('/careers', [App\Http\Controllers\Site\HomeController::class, 'careers'])->name('careers');
 Route::get('/blog', [App\Http\Controllers\Site\HomeController::class, 'blog'])->name('blog.list');
 Route::get('/blog/{slug}', [App\Http\Controllers\Site\HomeController::class, 'blogDetails'])->name('blog.details');
 Route::get('/blog/category/{url}',[App\Http\Controllers\Site\HomeController::class, 'blogCategory'])->name('category.blog');
